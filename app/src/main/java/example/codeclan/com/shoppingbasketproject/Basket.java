39df1e0c3c8e5cad11d70c390c2f79ab48f08e99
goobering <goobering@gmail.com>
2017-07-22 11:52:04 +0100

    Built out Basket class - TestBasket failing on getDiscountedValue methods as expected

--- app/src/main/java/example/codeclan/com/shoppingbasketproject/Basket.java
+++ app/src/main/java/example/codeclan/com/shoppingbasketproject/Basket.java
@@ -1,9 +1,42 @@
 package example.codeclan.com.shoppingbasketproject;
 
+import java.util.ArrayList;
+
 /**
  * Created by user on 22/07/2017.
  */
 
 public class Basket
 {
+    private ArrayList<Item> items;
+
+    public Basket()
+    {
+        items = new ArrayList<Item>();
+    }
+
+    public void addItem(Item item)
+    {
+        items.add(item);
+    }
+
+    public int getNumItems()
+    {
+        return items.size();
+    }
+
+    public boolean removeItem(Item item)
+    {
+        return items.remove(item);
+    }
+
+    public void empty()
+    {
+        items.clear();
+    }
+
+    public int getDiscountedValue()
+    {
+        return -1;
+    }
 }
