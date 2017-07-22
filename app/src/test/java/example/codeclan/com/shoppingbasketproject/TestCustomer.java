4151ef3731451f5140fb8c96aa378c4c076c293c
goobering <goobering@gmail.com>
2017-07-22 11:35:33 +0100

    Fleshed out TestCustomer class with basic test suite

---
+++ app/src/test/java/example/codeclan/com/shoppingbasketproject/TestCustomer.java
@@ -0,0 +1,44 @@
+package example.codeclan.com.shoppingbasketproject;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Created by user on 22/07/2017.
+ */
+
+public class TestCustomer
+{
+    private Customer customer;
+
+    @Before
+    public void before()
+    {
+        customer = new Customer();
+    }
+
+    @Test
+    public void testCreateCustomer()
+    {
+        assertEquals("Customer", customer.getClass().getSimpleName());
+    }
+
+    @Test
+    public void testAddLoyaltyCard()
+    {
+        LoyaltyCard loyaltyCard = new LoyaltyCard();
+        customer.addLoyaltyCard(loyaltyCard);
+        assertEquals(loyaltyCard, customer.getLoyaltyCard());
+    }
+
+    @Test
+    public void testAddBasket()
+    {
+        Basket basket = new Basket();
+        customer.addBasket(basket);
+        assertEquals(basket, customer.getBasket());
+    }
+}
