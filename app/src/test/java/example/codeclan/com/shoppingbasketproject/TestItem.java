9a186ac697edf5e8539007d59e9a44968f3779c4
goobering <goobering@gmail.com>
2017-07-22 11:19:10 +0100

    Fleshed out ItemTest class with basic test suite

--- app/src/test/java/example/codeclan/com/shoppingbasketproject/TestItem.java
+++ app/src/test/java/example/codeclan/com/shoppingbasketproject/TestItem.java
@@ -1,9 +1,41 @@
 package example.codeclan.com.shoppingbasketproject;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by user on 22/07/2017.
  */
 
 public class TestItem
 {
+    private Item item;
+
+    @Before
+    public void before()
+    {
+        item = new Item();
+    }
+
+    @Test
+    public void testCreateItem()
+    {
+        assertEquals("Item", item.getClass().getSimpleName());
+    }
+
+    @Test
+    public void testSetName()
+    {
+        item.setName("testItem");
+        assertEquals("testItem", item.getName());
+    }
+
+    @Test
+    public void testSetValue()
+    {
+        item.setValue(100);
+        assertEquals(100, item.getValue());
+    }
 }
